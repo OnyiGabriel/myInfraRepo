82e010b672ff8572cb5e485a6a98c2fdf118c4b9
Ubuntu <ubuntu@ip-172-31-21-50.us-east-2.compute.internal>
2021-11-20 04:26:44 +0000

    Added terraform files

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+    description = "The AWS region to create things in."
+    default     = "us-east-2"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     = "MyEC2Key"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     = "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "my-jenkins-security-group"
+}
+
+variable "tag_name" {
+  description = "Tag name of for Ec2 instance"
+  default     = "my-ec2-instance"
+}
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-0b9064170e32bde34"
+} 
