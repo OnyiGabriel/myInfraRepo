4afb05ebd12aff6c57d1e74cf206169497c21fd1
OnyiGabriel <92948920+OnyiGabriel@users.noreply.github.com>
2021-11-24 17:47:38 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ resource "aws_security_group" "security_jenkins_grp" {
  }
 }
 
+# Create AWS ec2 instance
 resource "aws_instance" "myFirstInstance" {
  ami            = var.ami_id
  key_name       = var.key_name
